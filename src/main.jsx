f7e18887ce76eda1d3ac472e04980dcd97186321
Ankit Dubey <ankitdubey.aitech@gmail.com>
2026-07-02 18:37:28 +0530

    Latest fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,5 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import FinePrint from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <FinePrint />
-  </React.StrictMode>
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
